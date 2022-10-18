0e4747fe185db82f1428213ce2ddcc9a594c9f37
DanielDarwiche <danielinti1@hotmail.com>
2022-10-18 17:03:30 +0200

    Add project files.

---
+++ OOP Labb 1/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_1
+{
+    class Circle
+    {
+        public double radie;
+        public double pi = 3.141;       
+
+        public double getArea()
+        {
+            double area = radie * radie * pi;  
+            Console.WriteLine($"Arean av cirklen är: {area}");
+            Console.WriteLine();
+            return area;
+        }
+        public void getOmkrets()
+        {  
+            double diameter = radie * 2;
+            double omkrets = pi * diameter; 
+            Console.WriteLine($"Omkretsen av cirklen/cylindern (med {diameter} i diameter) är: {omkrets}");
+            Console.WriteLine();
+        }
+        public void getVolym()
+        {
+            double höjd = 10;
+            double volym = (radie*2) * höjd*pi;
+            Console.WriteLine($"Volymen av cirkeln/cylindern är {volym} (när höjden är {höjd} och diameter {radie*2})");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ OOP Labb 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP_Labb_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle nyForm = new Circle();
+            nyForm.radie = 5;
+            Circle storForm = new Circle();
+            storForm.radie = 6;
+
+            nyForm.getArea();
+            nyForm.getOmkrets();
+            nyForm.getVolym();
+            Console.WriteLine("-----------");
+            storForm.getArea();
+            storForm.getOmkrets();
+            storForm.getVolym();
+            Console.WriteLine("-----------");
+            Circle valFri = new Circle();
+            Console.WriteLine("Ange valfri radie på en cirkel för att se alla värden!");
+            valFri.radie = double.Parse(Console.ReadLine());
+            valFri.getArea();
+            valFri.getOmkrets();
+            valFri.getVolym();
+
+        }
+    }
+}
